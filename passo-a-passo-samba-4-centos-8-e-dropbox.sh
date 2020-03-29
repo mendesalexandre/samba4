da189895e7c0e9c35058ed513841f87944d1c181
Alexandre Teixeira Mendes <alex.fnvip@gmail.com>
2020-03-29 00:22:26 -0400

    Passo a Passo Centos-8 + Dropbox

--- passo-a-passo-samba-4-centos-8-e-dropbox.sh
+++ passo-a-passo-samba-4-centos-8-e-dropbox.sh
@@ -64,3 +64,50 @@ cd samba-4.12.0
 
 # Se todas as dependencias estiverem ok, vai ter aparecido um tela dizendo que a compilação ocorreu com sucesso!
 
+#Apesar de não ter provisionado o dominio, vamos configurar o path, deixa o samba-tool disponível, 
+# sem necessidade de passar o caminho absoluto do binário.
+vim ~/.bash_profile
+# Procure por
+PATH=$PATH:$HOME/bin
+
+#Acrescente o diretorio do samba, e deixe como o exemplo abaixo.
+PATH=$PATH:$HOME/bin:/opt/samba/sbin:/opt/samba/bin
+
+# Relendo o arquivo para as alterações entrarem em vigor. 
+source ~/.bash_profile
+
+# Editando o arquivo do named.conf e incluindo os arquivos de DNS do Samba
+vim /etc/named.conf
+
+		   #Adicione o IP do servidor samba para que a porta 53 escute.
+11 		   listen-on port 53 { 127.0.0.1; 192.168.254.4; };
+		   # Ativando o recursion/query dentro da Rede Local.
+20         allow-recursion { localhost; 192.168.254.0/24; };
+21         allow-query     { localhost; 192.168.254.0/24; };
+
+		   # Habilitando a chave do samba
+40         tkey-gssapi-keytab "/opt/samba/bind-dns/dns.keytab";
+41         minimal-responses yes;
+
+			# Arquivo com as zonas do samba
+64 include "/opt/samba/bind-dns/named.conf";
+
+
+#Criando um arquivo para inicializar o samba atráves do systemd e também automaticamente no boot do S.O
+vim /etc/systemd/system/samba.service
+
+[Unit]
+Description= Samba 4 Active Directory
+After=syslog.target
+After=network.target
+
+[Service]
+Type=forking
+PIDFile=/opt/samba/var/run/samba.pid # /usr/local/samba/var/run/samba.pid
+ExecStart=/opt/samba/sbin/samba # /usr/local/samba/sbin/samba
+
+[Install]
+WantedBy=multi-user.target
+
+
+
