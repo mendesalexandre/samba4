b724bf8300f543a393eb8af98b8218be2a64063e
Alexandre Teixeira Mendes <alex.fnvip@gmail.com>
2020-03-28 23:47:14 -0400

    Passo a Passo Centos-8 + Dropbox

---
+++ passo-a-passo.sh
@@ -0,0 +1,14 @@
+
+Instalação do 
+	  
+	  
+	  
+yum install docbook-style-xsl gcc gdb gnutls-devel jansson-devel \
+      keyutils-libs-devel krb5-workstation libacl-devel libaio-devel \
+      libattr-devel libblkid-devel libtasn1 libtasn1-tools \
+      libxml2-devel libxslt lmdb-devel openldap-devel pam-devel perl \
+      perl-ExtUtils-MakeMaker perl-Parse-Yapp popt-devel python3-cryptography \
+      python3-dns python3-gpg python36-devel readline-devel systemd-devel \
+      tar zlib-devel gpgme-devel libarchive-devel libicu-devel tracker glib2 glibc libtirpc-devel \
+	  bind bind-devel wget rsync rpcgen python3-docutils nautilus-devel git net-tools bind-utils bzip \
+	  nautilus-devel python3-docutils
